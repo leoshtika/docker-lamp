17e23e539d5a40bd3c73218c61aff23760ff53ae
Leonard Shtika <leoshtika@gmail.com>
2019-04-13 00:54:50 +0300

    Add a demo user app

--- sql-dump/appdb.sql
+++ sql-dump/appdb.sql
@@ -1,7 +1,71 @@
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: db
+-- Generation Time: Apr 12, 2019 at 09:42 PM
+-- Server version: 5.7.25
+-- PHP Version: 7.2.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `appdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `first` varchar(255) NOT NULL,
+  `last` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `first`, `last`, `email`) VALUES
+(1, 'John', 'Doe', 'johndoe@email.com'),
+(2, 'Jane', 'Doe', 'janedoe@email.com'),
+(3, 'Carl', 'Henson', 'carl@someemail.com'),
+(4, 'Kamren ', 'Hoffman', 'kamren@myemail.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- www/index.php
+++ www/index.php
@@ -1,3 +1,36 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-phpinfo();
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Users</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Users</h1>
+        <?php
+        $conn = mysqli_connect('db', 'admin', 'pass123', 'appdb');
+        $query = 'SELECT * FROM user';
+        $result = mysqli_query($conn, $query);
+        echo '<table class="table table-striped">';
+        echo '<thead><tr><th>Id</th><th>First</th><th>Last</th><th>Email</th></tr></thead>';
+        while ($value = $result->fetch_array(MYSQLI_ASSOC)) {
+            echo '<tr>';
+            foreach ($value as $element) {
+                echo '<td>' . $element . '</td>';
+            }
+            echo '</tr>';
+        }
+        echo '</table>';
+        $result->close();
+        mysqli_close($conn);
+        ?>
+    </div>
+</body>
+
+</html>
